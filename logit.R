c6b265422857efcbf1077ecc28ceea3b9c2a9e87
alexanderfrei <whutmail@gmail.com>
2017-03-05 01:13:26 +0300

    path structuring

--- logit.R
+++
@@ -1,7 +0,0 @@
-l <- read.table("data/logit.txt")
-l.logit <- glm(data=l, V2 ~ V1, family=binomial)
-summary(l.logit)
-
-pokaz <- read.table("data/pokaz.txt")
-pokaz.logit <- glm(data=pokaz, V3 ~ ., family=binomial)
-summary(pokaz.logit)
---
+++ mining/association rules.R
@@ -0,0 +1,32 @@
+library("arules")
+library("arulesViz")
+
+load("data/titanic.raw.rdata")
+
+titanic.raw
+
+rules <- apriori(titanic.raw,
+  parameter = list(minlen=2, supp=0.005, conf=0.8),
+  appearance = list(rhs=c("Survived=No", "Survived=Yes"), 
+  default="lhs"),
+  control = list(verbose=F))
+
+rules.sorted <- sort(rules, by="lift")
+inspect(rules.sorted)
+
+subset.matrix <- is.subset(rules.sorted, rules.sorted)
+subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
+redundant <- colSums(subset.matrix, na.rm=T) >= 1
+rules.pruned <- rules.sorted[!redundant]
+inspect(rules.pruned)
+
+### viz
+
+plot(rules) # scatterplot
+
+subrules2 <- head(sort(rules, by="lift"), 10) # get 10 rules with gihest lift 
+plot(subrules2, method="graph") # graph
+
+plot(rules, method="grouped") # matrix
+
+# saveAsGraph(head(sort(rules, by="lift"),20), file="rules.graphml")
