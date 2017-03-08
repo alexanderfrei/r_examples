3dafe5cd7d02a8b83553362bff566a7da5291338
alexanderfrei <whutmail@gmail.com>
2017-03-08 11:22:09 +0300

    lm

--- graphics/scatterplot_matrix.R
+++ graphics/scatterplot_matrix.R
@@ -1,5 +1,14 @@
+# with trend
 crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState2005.csv",
                   sep = ",", header = T)
-
 crime2 <- crime[!(crime$state %in% c("District of Columbia","United States")),]
 pairs(crime2[,2:9], panel = panel.smooth)
+
+# with density in diag
+sat.df <- read.csv("data/park.csv")
+library(car)
+scatterplotMatrix(sat.df)
+
+# with hist 
+library(gpairs)
+gpairs(sat.df)
